f578af3a85a8cef498090d01f42e94fc86c691c1
Mahan <mahanasadi2013@gmail.com>
2026-09-14 17:44:02 +0330

    Create calculator.py
    Add addition operation

---
+++ calculator.py
@@ -0,0 +1,7 @@
+while True:
+    num1 = float(input("First Number: "))
+    mo = input("Mathemical Operator: ")
+    num2 = float(input("Second Number: "))
+    print("—" * 41)
+    if mo == "+":
+        print(num1 + num2)
